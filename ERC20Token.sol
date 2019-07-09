30f6b6fff091f3a6285e5edc4867bc8df494f217
808token <51093325+808token@users.noreply.github.com>
2019-07-09 20:04:44 +0300

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,47 +1,123 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.19;
 
-contract Token808 {
+contract Token {
 
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
+    /// @return total amount of tokens
+    function totalSupply() constant returns (uint supply) {}
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping(address => uint256)) public allowance;
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) constant returns (uint balance) {}
 
-    event Transfer(address from, address to, uint256 value);
-    event Approval(address from, address to, uint256 value);
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint _value) returns (bool success) {}
 
-    function Token808(){
-        decimals = 6;
-        totalSupply = 8080808080808 * (10 ** uint256(decimals));
-        balanceOf[msg.sender] = totalSupply;
-        name = "808token";
-        symbol = "808T";
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint _value) returns (bool success) {}
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint _value) returns (bool success) {}
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+contract RegularToken is Token {
+
+    function transfer(address _to, uint _value) returns (bool) {
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        if (balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != 0x0);
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
-        balanceOf[_to] += _value;
-        balanceOf[_from] -= _value;
-        Transfer(_from, _to, _value);
+    function transferFrom(address _from, address _to, uint _value) returns (bool) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
     }
 
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
+    function balanceOf(address _owner) constant returns (uint) {
+        return balances[_owner];
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public {
-        require(_value <= allowance[_from][_to]);
-        allowance[_from][_to] -= _value;
-        _transfer(_from, _to, _value);
+    function approve(address _spender, uint _value) returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
     }
 
-    function approve(address _to, uint256 _value) public {
-        allowance[msg.sender][_to] = _value;
-        Approval(msg.sender, _to, _value);
+    function allowance(address _owner, address _spender) constant returns (uint) {
+        return allowed[_owner][_spender];
+    }
+
+    mapping (address => uint) balances;
+    mapping (address => mapping (address => uint)) allowed;
+    uint public totalSupply;
+}
+
+contract UnboundedRegularToken is RegularToken {
+
+    uint constant MAX_UINT = 2**256 - 1;
+    
+    /// @dev ERC20 transferFrom, modified such that an allowance of MAX_UINT represents an unlimited amount.
+    /// @param _from Address to transfer from.
+    /// @param _to Address to transfer to.
+    /// @param _value Amount to transfer.
+    /// @return Success of transfer.
+    function transferFrom(address _from, address _to, uint _value)
+        public
+        returns (bool)
+    {
+        uint allowance = allowed[_from][msg.sender];
+        if (balances[_from] >= _value
+            && allowance >= _value
+            && balances[_to] + _value >= balances[_to]
+        ) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            if (allowance < MAX_UINT) {
+                allowed[_from][msg.sender] -= _value;
+            }
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+contract Token808TA is UnboundedRegularToken {
+
+    uint8 constant public decimals = 18;
+    uint public totalSupply =  8080808080808 * (10 ** uint256(decimals));
+    string constant public name = "808TA";
+    string constant public symbol = "808TA";
+
+    function Token808TA() {
+        balances[msg.sender] = totalSupply;
+        Transfer(address(0), msg.sender, totalSupply);
     }
 }
